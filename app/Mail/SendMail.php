54e71bc9e1cb539e95fb77c1df1f27bbb23b9d34
Dilovar Azimov <dilovar09@gmail.com>
2020-07-31 12:48:28 +0300

    files

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -40,7 +40,7 @@ class SendMail extends Mailable
                 'phone' => $this->phone,
                 'transport' => $this->transport,
             ]);
-            if (isset($this->$files)){
+            if (count($this->$files)>0){
             foreach($this->$files as $file) {
                     $message->attach($file->getRealPath(), array(
                         'as' => $file->getClientOriginalName(), // If you want you can chnage original name to custom name      
